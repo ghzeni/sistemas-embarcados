5d0c063b82950c8b24fea3f7b60f466412fd8697
cheddargt <gus_ghz@live.com>
2025-04-13 22:06:02 -0300

    update tivaware

---
+++ projetos/TivaWare_C_Series-2.2.0.295/examples/boards/ek-tm4c1294xl/hello/rvmdk/pinout.d
@@ -0,0 +1,5 @@
+./rvmdk/pinout.o: ..\drivers\pinout.c ..\..\..\..\inc\hw_gpio.h \
+  ..\..\..\..\inc\hw_memmap.h ..\..\..\..\inc\hw_types.h \
+  ..\..\..\..\driverlib\gpio.h ..\..\..\..\driverlib\pin_map.h \
+  ..\..\..\..\driverlib\rom.h ..\..\..\..\driverlib\rom_map.h \
+  ..\..\..\..\driverlib\sysctl.h ..\drivers\pinout.h
